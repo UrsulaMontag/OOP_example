cfa59690fd08df5cf7e88abcf6f9dae42a006733
ugla <montagu666@gmail.com>
2024-05-29 15:54:54 +0200

    Create library class and instance

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,21 @@ public class Main {
         myCar.startCar();
 
         Car yourCar = new Car("Subaru", "Forester", "silver", 1998);
-        yourCar.startCar();
-        yourCar.accelerateSpeed(25);
-        yourCar.accelerateSpeed(25);
-        System.out.println(yourCar.getSpeed());
+//        yourCar.startCar();
+//        yourCar.accelerateSpeed(25);
+//        yourCar.accelerateSpeed(25);
+//        System.out.println(yourCar.getSpeed());
 
+        // Exercise OOP-II
         Book myBook = new Book("Bios", "343-343-3434-3434", "Daniel Suarez");
+        Book myBook1 = new Book("Deamon", "343-343-999-3434", "Daniel Suarez");
+        Book myBook2 = new Book("Darknet", "343-343-666-3434", "Daniel Suarez");
         System.out.println(myBook);
+
+        Book[] myBooks = {myBook, myBook1, myBook2};
+
+        Library myLibrary = new Library(myBooks);
+        System.out.println(myLibrary);
 
     }
 
